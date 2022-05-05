f51fe7d6026521c50e7895df5df1c6841c83dc91
AF1QUE <afique.dev@gmail.com>
2022-05-05 18:05:36 +0600

    Added tailwind css and restructured HTML

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        violet: "#7541c8",
+        grayishBlue: "#48556a",
+        blackishBlue: "#19212e",
+        gray: "#cfcfcf",
+        lightGray: "#ecf2f8",
+      },
+    },
+  },
+  plugins: [],
+};
