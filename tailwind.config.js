7002787de39298a456eec9da681e82a0d311ecf4
AF1QUE <afique.dev@gmail.com>
2022-05-10 02:26:00 +0600

    Initialized core dependencies, created HTML file with content

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    colors: {
+      violet: "hsl(263, 55%, 52%)",
+      grayishBlue: "hsl(217, 19%, 35%)",
+      blackishBlue: "hsl(219, 29%, 14%)",
+      white: "hsl(0, 0%, 100%)",
+      lightGray: "hsl(0, 0%, 81%)",
+      lightGrayishBlue: "hsl(210, 46%, 95%)",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
