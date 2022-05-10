fcf49b03f57fd8c9bca564b4fd903f8d8fe1e9ae
AF1QUE <afique.dev@gmail.com>
2022-05-10 21:15:05 +0600

    Finihsed building the mobile version

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,22 @@ module.exports = {
       lightGray: "hsl(0, 0%, 81%)",
       lightGrayishBlue: "hsl(210, 46%, 95%)",
     },
+    screens: {
+      "2xl": { max: "1535px" },
+      // => @media (max-width: 1535px) { ... }
+
+      xl: { max: "1279px" },
+      // => @media (max-width: 1279px) { ... }
+
+      lg: { max: "1023px" },
+      // => @media (max-width: 1023px) { ... }
+
+      md: { max: "767px" },
+      // => @media (max-width: 767px) { ... }
+
+      sm: { max: "639px" },
+      // => @media (max-width: 639px) { ... }
+    },
     extend: {},
   },
   plugins: [],
